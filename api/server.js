46188b7d96e2002e84560e46303ed4f3e67536a8
Eduardo Lopez <ed98lopez@gmail.com>
2021-11-11 19:25:56 -0800

    commented out some unneeded code. Think the db stuff if all in order

--- api/server.js
+++ api/server.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const helmet = require('helmet')
 const cors = require('cors')
-const db = require('./data/db-config')
+//const db = require('./data/db-config')
 
 //const userRoutes = require('./users/userRoutes')
 
-function getAllUsers() { return db('users') }
+/* function getAllUsers() { return db('users') }
 
 async function insertUser(user) {
   // WITH POSTGRES WE CAN PASS A "RETURNING ARRAY" AS 2ND ARGUMENT TO knex.insert/update
@@ -13,7 +13,7 @@ async function insertUser(user) {
   // UNLIKE SQLITE WHICH FORCES US DO DO A 2ND DB CALL
   const [newUserObject] = await db('users').insert(user, ['user_id', 'username', 'password'])
   return newUserObject // { user_id: 7, username: 'foo', password: 'xxxxxxx' }
-}
+} */
 
 const server = express()
 server.use(express.json())
@@ -22,12 +22,12 @@ server.use(cors())
 
 //server.use('/users',userRoutes)
 
-server.get('/api/users', async (req, res) => {
+/* server.get('/api/users', async (req, res) => {
   res.json(await getAllUsers())
 })
 
 server.post('/api/users', async (req, res) => {
   res.status(201).json(await insertUser(req.body))
 })
-
+ */
 module.exports = server
