d56cd683be6c3b797a588067b3b3139e8e679832
Eduardo Lopez <ed98lopez@gmail.com>
2021-11-13 19:40:11 -0800

    commit

--- api/data/migrations/20211114004634_status_table.js
+++ api/data/migrations/20211114004634_status_table.js
@@ -1,14 +1,10 @@
-
-exports.up = async function(knex) {
-    await knex.schema
-        .createTable('statuses', (statuses)=>{
-            statuses.increments('status_id')
-            statuses.string('status')
-        })
-  
+exports.up = async function (knex) {
+  await knex.schema.createTable("statuses", (statuses) => {
+    statuses.increments("status_id");
+    statuses.string("status");
+  });
 };
 
-exports.down = async function(knex) {
-    await knex.dropTableIfExists('statuses')
-  
+exports.down = async function (knex) {
+  await knex.schema.dropTableIfExists("statuses");
 };
--- api/data/migrations/20211114004737_homes_table.js
+++ api/data/migrations/20211114004737_homes_table.js
@@ -1,17 +1,19 @@
-
-exports.up = async function(knex) {
-  await knex.schema.createTable('homes',(homes)=>{
-    homes.increments('home_id')
-    homes.string('address',200).notNullable()
-    homes.integer('postal_code').notNullable()
-    homes.string('nickname',200).notNullable()
-    homes.string('notes')
-    homes.integer('status_id').reference('status_id').inTable('statuses').defaultTo(1)
-    homes.string('city',200).notNullable()
-
-})
+exports.up = async function (knex) {
+  await knex.schema.createTable("homes", (homes) => {
+    homes.increments("home_id");
+    homes.string("nickname", 200).notNullable();
+    homes.string("address", 200).notNullable();
+    homes.string("city", 200).notNullable();
+    homes.integer("postal_code").notNullable();
+    homes.string("notes");
+    homes
+      .integer("status_id")
+      .references("status_id")
+      .inTable("statuses")
+      .defaultTo(1);
+  });
 };
 
-exports.down = async function(knex) {
-  await knex.dropTableIfExists('homes')
+exports.down = async function (knex) {
+  await knex.schema.dropTableIfExists("homes");
 };
