f4848a5c1a3e095301bc21069c820ad6830d99f7
Eduardo Lopez <ed98lopez@gmail.com>
2021-11-14 18:47:39 -0800

    formatting

--- api/routers/homeRoute/homeRoutes.js
+++ api/routers/homeRoute/homeRoutes.js
@@ -1,5 +1,9 @@
 const express = require("express");
-const { getResources, getResourceById, addResource } = require("../../models/dbHelpers");
+const {
+  getResources,
+  getResourceById,
+  addResource,
+} = require("../../models/dbHelpers");
 const router = express.Router();
 /*
 //middleware for validation
@@ -31,21 +35,21 @@ router.get("/", async (req, res) => {
 router.get("/:id", async (req, res) => {
   const id = req.params.id;
   try {
-    const home = await getResourceById('homes',id);
-    console.log(home)
+    const home = await getResourceById("homes", id);
+    console.log(home);
     res.status(200).json(home);
   } catch (error) {
     res.status(500).json("internal server error");
   }
 });
 
-router.post('/', async (req,res)=>{
-    try{
-        const homes = await addResource('homes',req.body)
-        res.status(200).json(homes)
-    }catch(error){
-        res.status(500).json('oops')
-    }
-})
+router.post("/", async (req, res) => {
+  try {
+    const homes = await addResource("homes", req.body);
+    res.status(200).json(homes);
+  } catch (error) {
+    res.status(500).json("oops");
+  }
+});
 
 module.exports = router;
