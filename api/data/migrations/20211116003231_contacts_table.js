96a7f0c35d62c2e1037516050031cef33948d97f
Eduardo Lopez <ed98lopez@gmail.com>
2021-11-15 19:16:54 -0800

    added repair and images table

--- api/data/migrations/20211116003231_contacts_table.js
+++ api/data/migrations/20211116003231_contacts_table.js
@@ -1,30 +1,50 @@
+exports.up = async function (knex) {
+  await knex.schema.createTable("contacts", (contacts) => {
+    contacts.increments("contact_id");
+    contacts
+      .integer("home_id")
+      .unsigned()
+      .notNullable()
+      .references("home_id")
+      .inTable("homes")
+      .onDelete("CASCADE");
+    contacts.string("first", 200).notNullable();
+    contacts.string("last", 200).nullable();
+    contacts.string("phone", 10);
+    contacts.string("email", 200);
+    contacts.string("mail_address", 200);
+    contacts.string("notes", 1000);
+  });
 
-exports.up = async function(knex) {
-    await knex.schema.createTable('contacts',(contacts)=>{
-        contacts.increments('contact_id')
-        contacts.integer('home_id').unsigned().notNullable().references('home_id').inTable('homes').onDelete('CASCADE')
-        contacts.string('first',200).notNullable()
-        contacts.string('last',200).nullable()
-        contacts.string('phone',10)
-        contacts.string('email',200)
-        contacts.string('mail_address',200)
-        contacts.string('notes',1000)
-    })
-
-    await knex.schema.createTable('contact_log',(cLog)=>{
-        cLog.increments('c_log_id')
-        cLog.integer('home_id').unsigned().notNullable().references('home_id').inTable('homes').onDelete('RESTRICT')
-        cLog.integer('contact_id').unsigned().notNullable().references('contact_id').inTable('contacts').onDelete('RESTRICT')
-        cLog.integer('user_id').unsigned().notNullable().references('user_id').inTable('users').onDelete('RESTRICT')
-        cLog.string('notes',1000)
-        cLog.timestamps(false,true)
-
-    })
-  
+  await knex.schema.createTable("contact_log", (cLog) => {
+    cLog.increments("c_log_id");
+    cLog
+      .integer("home_id")
+      .unsigned()
+      .notNullable()
+      .references("home_id")
+      .inTable("homes")
+      .onDelete("RESTRICT");
+    cLog
+      .integer("contact_id")
+      .unsigned()
+      .notNullable()
+      .references("contact_id")
+      .inTable("contacts")
+      .onDelete("RESTRICT");
+    cLog
+      .integer("user_id")
+      .unsigned()
+      .notNullable()
+      .references("user_id")
+      .inTable("users")
+      .onDelete("RESTRICT");
+    cLog.string("notes", 1000);
+    cLog.timestamps(false, true);
+  });
 };
 
-exports.down = async function(knex) {
-    await knex.schema.dropTableIfExists('contact_log')
-    await knex.schema.dropTableIfExists('contacts')
- 
+exports.down = async function (knex) {
+  await knex.schema.dropTableIfExists("contact_log");
+  await knex.schema.dropTableIfExists("contacts");
 };
---
+++ api/data/migrations/20211116015645_repairs_table.js
@@ -0,0 +1,40 @@
+exports.up = async function (knex) {
+  await knex.schema.createTable("images", (images) => {
+    images.increments("image_id");
+    images
+      .integer("home_id")
+      .unsigned()
+      .notNullable()
+      .references("home_id")
+      .inTable("homes")
+      .onDelete("RESTRICT");
+    images.binary("image").notNullable();
+  });
+
+  await knex.schema.createTable("repairs", (repairs) => {
+    repairs.increments("repair_id");
+    repairs.string("title", 200).notNullable();
+    repairs.string("description", 2000);
+    repairs.boolean("completed").defaultTo(false);
+    repairs.integer("cost");
+    repairs
+      .integer("home_id")
+      .unsigned()
+      .notNullable()
+      .references("home_id")
+      .inTable("homes")
+      .onDelete("RESTRICT");
+    repairs
+      .integer("image_id")
+      .unsigned()
+      .notNullable()
+      .references("image_id")
+      .inTable("images")
+      .onDelete("RESTRICT");
+  });
+};
+
+exports.down = async function (knex) {
+  await knex.schema.dropTableIfExists("repairs");
+  await knex.schema.dropTableIfExists("images");
+};
